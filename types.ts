a01d1cb6d154aad77fb79ef0f80726d114cacde2
Sean aye <hello@seanaye.ca>
2021-06-05 14:44:38 -0400

    support oak native request type

--- types.ts
+++ types.ts
@@ -1,6 +1,9 @@
-import { ServerRequest } from 'https://deno.land/std@0.97.0/http/server.ts'
+import { ServerRequest } from "https://deno.land/std@0.97.0/http/server.ts";
+import { NativeRequest } from "https://deno.land/x/oak@v7.5.0/http_server_native.ts";
 
 /**
  * Request type with only required properties
  */
-export type Request = Pick<ServerRequest, 'respond' | 'body' | 'method' | 'headers'>
+export type Request =
+  | Pick<ServerRequest, "respond" | "body" | "method" | "headers">
+  | NativeRequest;
