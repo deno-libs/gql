56a1c02896859042bf4c884c619b85d3d52fd44b
v1rtl <pilll.pl22@gmail.com>
2021-06-16 01:38:14 +0300

    bump deps

--- examples/abc.ts
+++ examples/abc.ts
@@ -1,6 +1,6 @@
 import { Application } from 'https://deno.land/x/abc@v1.3.2/mod.ts'
 import { GraphQLHTTP } from '../mod.ts'
-import { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.0/mod.ts'
+import { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.1/mod.ts'
 import { gql } from 'https://deno.land/x/graphql_tag@0.0.1/mod.ts'
 
 const app = new Application()
--- examples/tinyhttp.ts
+++ examples/tinyhttp.ts
@@ -1,6 +1,6 @@
 import { App } from 'https://deno.land/x/tinyhttp@0.1.12/mod.ts'
 import { GraphQLHTTP } from '../mod.ts'
-import { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.0/mod.ts'
+import { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.1/mod.ts'
 import { gql } from 'https://deno.land/x/graphql_tag@0.0.1/mod.ts'
 
 const typeDefs = gql`
