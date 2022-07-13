65d1a2fa6a833458583a98d789e7143fa5609f44
v1rtl <pilll.pl22@gmail.com>
2022-07-13 16:32:32 +0300

    move all types to types.ts, bump deps, remove tinyhttp example

--- http.ts
+++ http.ts
@@ -1,5 +1,5 @@
-import { runHttpQuery, GQLOptions, GraphQLParams } from './common.ts'
-import type { GQLRequest } from './types.ts'
+import { runHttpQuery } from './common.ts'
+import type { GQLRequest, GQLOptions, GraphQLParams } from './types.ts'
 
 /**
  * Create a new GraphQL HTTP middleware with schema, context etc
@@ -19,13 +19,9 @@ export function GraphQLHTTP<Req extends GQLRequest = GQLRequest, Ctx extends { r
 }: GQLOptions<Ctx, Req>) {
   return async (request: Req) => {
     const accept = request.headers.get('Accept') || ''
-    
-    const typeList = [
-      'text/html',
-      'text/plain',
-      'application/json',
-      '*/*'
-    ].map(contentType => ({ contentType, index: accept.indexOf(contentType) }))
+
+    const typeList = ['text/html', 'text/plain', 'application/json', '*/*']
+      .map((contentType) => ({ contentType, index: accept.indexOf(contentType) }))
       .filter(({ index }) => index >= 0)
       .sort((a, b) => a.index - b.index)
       .map(({ contentType }) => contentType)
@@ -37,7 +33,7 @@ export function GraphQLHTTP<Req extends GQLRequest = GQLRequest, Ctx extends { r
     }
 
     let params: Promise<GraphQLParams>
-    
+
     if (request.method === 'GET') {
       const urlQuery = request.url.substring(request.url.indexOf('?'))
       const queryParams = new URLSearchParams(urlQuery)
@@ -52,7 +48,7 @@ export function GraphQLHTTP<Req extends GQLRequest = GQLRequest, Ctx extends { r
             ...headers
           })
         })
-      } else if(typeList.length === 1 && typeList[0] === 'text/html') {
+      } else if (typeList.length === 1 && typeList[0] === 'text/html') {
         return new Response('Not Acceptable', { status: 406, headers: new Headers(headers) })
       } else if (queryParams.has('query')) {
         params = Promise.resolve({ query: queryParams.get('query') } as GraphQLParams)
@@ -69,8 +65,8 @@ export function GraphQLHTTP<Req extends GQLRequest = GQLRequest, Ctx extends { r
       const result = await runHttpQuery<Req, Ctx>(await params, options, { request })
 
       let contentType = 'text/plain'
-      
-      if(!typeList.length || typeList.includes('application/json') || typeList.includes('*/*'))
+
+      if (!typeList.length || typeList.includes('application/json') || typeList.includes('*/*'))
         contentType = 'application/json'
 
       return new Response(JSON.stringify(result, null, 2), {
