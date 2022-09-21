30e5e5d2d94688dca53113f329bd55838e346eda
Tyler <tyler.cheswick@carvana.com>
2022-09-21 13:01:33 -0700

    converted to router

--- examples/oak.ts
+++ examples/oak.ts
@@ -1,70 +1,64 @@
-import { Application } from 'https://deno.land/x/oak@v10.1.0/mod.ts'
-import { GraphQLHTTP } from '../mod.ts'
-import { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.2/mod.ts'
-import { gql } from 'https://deno.land/x/graphql_tag@0.0.1/mod.ts'
+import {
+  Application,
+  Router,
+  Middleware,
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { GraphQLHTTP } from "../mod.ts";
+import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
+import { gql } from "https://deno.land/x/graphql_tag@0.0.1/mod.ts";
 
 const typeDefs = gql`
   type Query {
     hello: String
   }
-`
+`;
 
 const resolvers = {
   Query: {
     hello: (_root: undefined, _args: unknown, ctx: { request: Request }) => {
-      return `Hello World! from ${ctx.request.url}`
-    }
-  }
-}
+      return `Hello World! from ${ctx.request.url}`;
+    },
+  },
+};
 
-const schema = makeExecutableSchema({ resolvers, typeDefs })
+const schema = makeExecutableSchema({ resolvers, typeDefs });
 
 const resolve = GraphQLHTTP({
   schema,
   graphiql: true,
-  context: request => ({ request })
-})
+  context: (request) => ({ request }),
+});
 
-const app = new Application()
-
-app.use((ctx, next) => {
+const returnGraphiql: Middleware = async (ctx) => {
   // Allow CORS:
   // ctx.response.headers.append('access-control-allow-origin', '*')
   // ctx.response.headers.append('access-control-allow-headers', 'Origin, Host, Content-Type, Accept')
+  const req = ctx.request.originalRequest.request;
+  const res = await resolve(req);
 
-  if(ctx.request.method !== 'OPTIONS')
-    return next()
+  for (const [k, v] of res.headers.entries()) ctx.response.headers.append(k, v);
 
-  ctx.response.status = 204;
+  ctx.response.status = res.status;
+  ctx.response.body = res.body;
+};
 
-  if(ctx.request.url.pathname.startsWith('/graphql'))
-    ctx.response.headers.append('allow', 'OPTIONS, GET, POST')
-  else
-    ctx.response.headers.append('allow', '*')
-})
+const graphqlRouter = new Router()
+  .get("/graphql", returnGraphiql)
+  .options("/graphql", returnGraphiql)
+  .post("/graphql", returnGraphiql);
 
-app.use(async (ctx, next) => {
-  if(!ctx.request.url.pathname.startsWith('/graphql'))
-    return next()
+const app = new Application().use(
+  graphqlRouter.routes(),
+  graphqlRouter.allowedMethods()
+);
 
-  const req = ctx.request.originalRequest.request
-  const res = await resolve(req)
+app.addEventListener("listen", ({ secure, hostname, port }) => {
+  if (hostname === "0.0.0.0") hostname = "localhost";
 
-  for(const [k, v] of res.headers.entries())
-    ctx.response.headers.append(k, v)
+  const protocol = secure ? "https" : "http";
+  const url = `${protocol}://${hostname ?? "localhost"}:${port}`;
 
-  ctx.response.status = res.status
-  ctx.response.body = res.body
-})
+  console.log("☁  Started on " + url);
+});
 
-app.addEventListener('listen', ({ secure, hostname, port }) => {
-  if(hostname === '0.0.0.0')
-    hostname = 'localhost';
-
-  const protocol = secure ? 'https' : 'http'
-  const url = `${protocol}://${hostname ?? 'localhost'}:${port}`
-
-  console.log('☁  Started on ' + url)
-})
-
-await app.listen({ port: 3000 })
+await app.listen({ port: 3000 });
