cbb22a400e0ebc9b5459d7f93fcc046cdf5c0eca
unfocused <yudhvirraj@outlook.com>
2023-03-21 23:04:59 -0700

    updated deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,9 @@
-export { graphql } from 'https://esm.sh/graphql@16.6.0/graphql?target=denoNext'
+// export { graphql } from 'https://esm.sh/graphql@16.6.0/graphql?target=denoNext'
 export type { GraphQLArgs } from 'https://esm.sh/graphql@16.6.0/graphql?target=denoNext'
 export type { ExecutionResult } from 'https://esm.sh/graphql@16.6.0/execution/execute?target=denoNext'
 export type { GraphQLSchema } from 'https://esm.sh/graphql@16.6.0/type/schema?target=denoNext'
+
+export { graphql } from 'https://esm.sh/graphql@16.6.0/graphql?target=deno'
+// export type { GraphQLArgs } from 'https://esm.sh/graphql@16.6.0/graphql'
+// export type { ExecutionResult } from 'https://esm.sh/graphql@16.6.0/execution/execute'
+// export type { GraphQLSchema } from 'https://esm.sh/graphql@16.6.0/type/schema'
