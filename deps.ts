3af7bee88da35403fbd13c9b6ad6d13009ede726
unfocused <yudhvirraj@outlook.com>
2023-03-21 23:24:30 -0700

    denoNext to deno

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { graphql } from 'https://esm.sh/graphql@16.6.0/graphql?target=denoNext'
+export { graphql } from 'https://esm.sh/graphql@16.6.0/graphql?target=deno'
 export type { GraphQLArgs } from 'https://esm.sh/graphql@16.6.0/graphql?target=denoNext'
 export type { ExecutionResult } from 'https://esm.sh/graphql@16.6.0/execution/execute?target=denoNext'
 export type { GraphQLSchema } from 'https://esm.sh/graphql@16.6.0/type/schema?target=denoNext'
