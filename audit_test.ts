3d0139093de81e7db01439f023c7932bb956f75e
v1rtl <hi@v1rtl.site>
2023-11-08 23:57:40 +0200

    fix audit tests failing and remove uneeded args in ci

--- audit_test.ts
+++ audit_test.ts
@@ -1,12 +1,11 @@
-import { serverAudits } from 'npm:graphql-http'
+import { serverAudits } from 'npm:graphql-http@1.22.0'
 
 for (
   const audit of serverAudits({
     url: 'http://localhost:3000/graphql',
-    fetchFn: fetch,
   })
 ) {
-  Deno.test(audit.name, async () => {
+  Deno.test(audit.name, { sanitizeResources: false }, async () => {
     const result = await audit.fn()
     if (result.status === 'error') {
       throw result.reason
